b69fda14b1fc4ad4e756254081c4bee48fc2c796
MXY156018 <1132022296@qq.com>
2020-07-08 18:46:43 +0800

    '推荐信息'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,44 +4,153 @@
       <div slot="center">购物街</div>
     </nav-bar>
     <home-swiper :banners="banners"/>
+    <recommend-view :recomments='recommends'/>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import NavBar from 'components/common/navbar/NavBar'
+
   import HomeSwiper from './childComps/HomeSwiper'
-  import {getHomeMultidata} from "network/home";
+  import RecommendView from "./childComps/RecommendView";
+  import FeatureView from './childComps/FeatureView'
+
+  import NavBar from 'components/common/navbar/NavBar'
+  import TabControl from "components/content/tabControl/TabControl";
+
+
+  import {
+    getHomeMultidata,
+    getHomeGoods
+  } from "network/home";
 
 
   export default {
     name: "Home",
     components: {
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
       NavBar,
-      HomeSwiper
+      TabControl
     },
     data() {
       return {
         banners: [],
         recommends: [],
-
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []}
+        }
       }
     },
     created() {
-      // 请求多个数据
-      getHomeMultidata().then(res => {
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      //请求多个数据
+      this.getHomeMultidata()
+      //请求分类数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+
+    },
+    methods: {
+      getHomeMultidata() {
+        // 请求多个数据
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1
+        getHomeGoods('type', page).then(res => {
+           res.data.list
+        })
+      },
+      tabClick(index) {
+        switch (index) {
+          case 0:
+            this.currentType = POP
+            break
+          case 1:
+            this.currentType = NEW
+            break
+          case 2:
+            this.currentType = SELL
+            break
+        }
+      }
     }
   }
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .nav-bar {
     background-color: var(--color-tint);
     font-weight: 700;
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
   }
 
+  .tab-control {
+    position: -webkit-sticky;
+    position: sticky;
+    top: 44px;
+    background-color: #fff;
+
+  }
 
 </style>
